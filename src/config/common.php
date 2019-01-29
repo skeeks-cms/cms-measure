55f55f80eb1341d70917a6c2aec9d8627de4b386
semenov <semenov@skeeks.com>
2019-01-29 19:08:52 +0300

    dev

---
+++ src/config/common.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 27.08.2015
+ */
+return [
+
+    'components' => [
+        'measure' => [
+            'class' => 'skeeks\cms\measure\components\MeasureComponent',
+        ],
+
+        'i18n' => [
+            'translations' => [
+                'skeeks/measure' => [
+                    'class'    => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@skeeks/cms/measure/messages',
+                    'fileMap'  => [
+                        'skeeks/measure' => 'main.php',
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
---
+++ src/config/web.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+return [
+    'modules' => [
+        'measure' => [
+            'class'         => 'skeeks\cms\measure\Module',
+        ]
+    ]
+];
